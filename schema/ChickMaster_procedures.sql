9dcd27e7bf2f738a59673709b637a7197aa305de
dsdar <david.reynolds@dsdar.com>
2020-08-04 10:22:34 -0400

    udpate procedures

---
+++ schema/ChickMaster_procedures.sql
@@ -0,0 +1,174 @@
+-- ==================================================================
+-- call IMAGES_getImageLayer('LOCATION',1,1,1,1);
+-- call IMAGES_getImageLayer('ASSET',1,1,1,1);
+-- call IMAGES_getImageLayer('ASSET-PART',1,1,1,1);
+
+
+DROP procedure IF EXISTS `IMAGES_getImageLayer`;
+
+DELIMITER $$
+CREATE  PROCEDURE IMAGES_getImageLayer(
+IN _action VARCHAR(100),
+IN _customerId VARCHAR(32),
+IN _userId VARCHAR(32),
+IN _startingPoint INT,
+IN _id INT
+)
+IMAGES_getImageLayer: BEGIN
+
+
+IF (_id is NULL OR _id='') THEN
+	SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Invalid value id';
+ELSEIF (_action is NULL OR _action='') THEN
+	SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Invalid value action';
+END IF;
+
+-- isPrimary may be enumerated (i.e 1,2,3 )  
+--   where 1=top most
+-- 			2 = layer down
+-- 			3 = layer down again
+--  	The reason is maybe the top layer is all site locations, but we really want to start the user
+-- 		at a more reasonable layer such as inside their facility.  We can store the layer start
+-- 		position as part of a preference
+
+if (_customerId is null) THEN
+	SIGNAL SQLSTATE '45001' SET MESSAGE_TEXT = 'customerId required';
+END IF;
+
+
+If (_action = 'LOCATION') THEN
+
+	if (_startingPoint is not null) THEN
+
+		SELECT loc.*
+		from location loc 
+        where loc.location_customerUUID = _customerId AND loc.location_isPrimary = _startingPoint;
+	
+	ELSE
+
+		SELECT loc.*
+		from location loc where loc.locationUUID = _id;
+	
+	END IF;
+
+ELSEIF (_action = 'ASSET') THEN
+
+	select a.*,p.* from asset a
+	left join asset_part p on (a.asset_partUUID = p.asset_partUUID) 
+    where assetUUID=_id;
+
+ELSEIF (_action = 'ASSET-PART') THEN
+
+	SELECT ap.* from asset_part ap where ap.asset_partUUID = _id;
+
+END IF;
+
+END$$
+
+
+-- ==================================================================
+-- call DIAGNOSTIC_getNode(null,1,1,'633a54011d76432b9fa18b0b6308c189',null); -- will get the starting tree node
+-- call DIAGNOSTIC_getNode(null,1,1,null,'1834487471bb4cccbaa8b0dc1cedc463'); -- will get the next node
+
+
+DROP procedure IF EXISTS `DIAGNOSTIC_getNode`;
+
+DELIMITER $$
+CREATE  PROCEDURE DIAGNOSTIC_getNode(
+IN _action VARCHAR(100),
+IN _customerId char(32),
+IN _userId char(32),
+IN _diagnosticId char(32),
+IN _nodeId char(32)
+)
+DIAGNOSTIC_getNode: BEGIN
+
+
+
+-- isPrimary may be enumerated (i.e 1,2,3 )  
+--   where 1=top most
+-- 			2 = layer down
+-- 			3 = layer down again
+--  	The reason is maybe the top layer is all site locations, but we really want to start the user
+-- 		at a more reasonable layer such as inside their facility.  We can store the layer start
+-- 		position as part of a preference
+
+if (_customerId is null) THEN
+	SIGNAL SQLSTATE '45001' SET MESSAGE_TEXT = 'customerId required';
+END IF;
+
+
+If (_diagnosticId is not null and _nodeId is null) THEN
+
+	-- get the starting node
+
+		SELECT n.*,d.* from diagnostic_tree d
+        left join diagnostic_node n on (d.diagnosticUUID=n.diagnostic_node_diagnosticUUID and d.diagnostic_startNodeUUID = n.diagnostic_nodeUUID) 
+        where diagnosticUUID = _diagnosticId;
+
+ELSEIF (_nodeId is not null) THEN
+
+		SELECT n.* from diagnostic_node n
+        -- left join diagnostic_tree d on (d.diagnosticUUID=n.diagnostic_node_diagnosticUUID) 
+        where diagnostic_nodeUUID = _nodeId;
+
+END IF;
+
+END$$
+
+
+-- ==================================================================
+-- call BUTTON_options(null,'1c5a0f1a10b841699ee5b5f431d01e03','CONTACT|CHAT|STARTCHECKLIST|ADDLOG'); 
+
+DROP procedure IF EXISTS `BUTTON_options`;
+
+DELIMITER $$
+CREATE  PROCEDURE BUTTON_options(
+IN _action VARCHAR(100),
+IN _asset_partUUID char(32),
+IN _otherOptions varchar(255)
+)
+BUTTON_options: BEGIN
+
+-- TODO, select security to turn on/off
+-- 'CONTACT,CHAT,STARTCHECKLIST,ADDLOG'
+select ap.asset_partUUID,
+(case when asset_part_isPurchasable=1 is not null then 1 else 0 end) BUTTON_viewOrderParts,
+(case when asset_part_diagnosticUUID is not null then 1 else 0 end) as BUTTON_diagnoseAProblem,
+(select count(apaj_asset_partUUID) from  asset_part_attachment_join where apaj_asset_partUUID = ap.asset_partUUID limit 1) as BUTTON_viewManual,
+(select count(pkj_part_partUUID) from part_knowledge_join where pkj_part_partUUID = ap.asset_partUUID limit 1 ) as BUTTON_qa,
+(select count(wapj_asset_partUUID) from workorder_asset_part_join where wapj_asset_partUUID = ap.asset_partUUID limit 1 ) as BUTTON_serviceHistory,
+(case when locate('CONTACT',_otherOptions)>0 THEN 1 ELSE 0 END) as BUTTON_contactCheckMaster,
+(case when locate('CHAT',_otherOptions)>0 THEN 1 ELSE 0 END) as BUTTON_liveChat,
+(case when locate('STARTCHECKLIST',_otherOptions)>0 THEN 1 ELSE 0 END) as BUTTON_startAChecklist,
+(case when locate('ADDLOG',_otherOptions)>0 THEN 1 ELSE 0 END) as BUTTON_addALogEntry,
+ap.*
+from asset_part ap
+where asset_partUUID=_asset_partUUID;
+
+END$$
+
+
+-- ==================================================================
+-- call NOTIFICATION_notification('GET','1',null); 
+-- call NOTIFICATION_notification('ACKNOWLEDGE','1',1001); 
+
+DROP procedure IF EXISTS `NOTIFICATION_notification`;
+
+DELIMITER $$
+CREATE  PROCEDURE NOTIFICATION_notification(
+IN _action VARCHAR(100),
+IN _userUUID char(32),
+IN _notificationId INT
+)
+NOTIFICATION_notification: BEGIN
+
+-- TODO, need to discuss acknowledgement and history.  It is not really the same as our notificaiton_queue
+
+if (_action='GET') THEN
+	select 'There was a flag on the last PRE-SET CHECKLIST';
+ELSEIF (_action='ACKNOWLEDGE') THEN
+	select 1001;
+END IF;
+
+END$$
